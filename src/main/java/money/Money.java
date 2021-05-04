2a3b74dca3968f5ce116622e097b671cf547cbc2
uraven0107 <turn.pso@gmail.com>
2021-05-04 13:50:22 +0900

    :pencil: chapter7 実装を隠す

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-public class Money {
+abstract class Money {
     protected int amount;
 
     public boolean equals(Object o) {
@@ -8,4 +8,14 @@ public class Money {
         return this.amount == money.amount
                 && getClass().equals(money.getClass());
     }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 }
