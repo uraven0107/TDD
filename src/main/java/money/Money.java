def1bd09d68626a1c43a6c0942d435bd273acecf
uraven0107 <turn.pso@gmail.com>
2021-05-04 19:36:40 +0900

    :pencil: chapter9 歩幅の調整

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -2,6 +2,12 @@ package money;
 
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    Money (int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public boolean equals(Object o) {
         Money money = (Money) o;
@@ -10,11 +16,15 @@ abstract class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    String currency() {
+        return this.currency;
     }
 
     abstract Money times(int multiplier);
